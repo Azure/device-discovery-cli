eb3748ef2f351e6016c0f50caebb5ea0d0d7a148
Zhijun Zhao <zhijzhao@microsoft.com>
2016-09-08 09:47:30 +0800

    Add missed files

---
+++ src/lib/ethernet-transport.js
@@ -0,0 +1,78 @@
+'use-strict';
+
+var arp = require('node-arp');
+var dns = require('dns');
+var mdns = require('mdns-js');
+var vow = require('vow');
+
+var OuiLookup = require('./oui-lookup');
+
+function resolveIpAddressToHostNameAsync(address) {
+  return new vow.Promise(function(resolve, reject /*, notify */) {
+    dns.reverse(address, function(err, domains) {
+      if(err) {
+        reject(err);
+        return;
+      }
+      resolve(domains);
+    });
+  });
+}
+
+function resolveIpAddressToMacAddressAsync(address) {
+  return new vow.Promise(function(resolve, reject /*, notify */) {
+    arp.getMAC(address, function(err, mac) {
+      if(err) {
+        reject(err);
+        return;
+      }
+      resolve(mac);
+    });
+  });
+}
+
+exports.beginDiscovery = function beginDiscovery(serviceType, callback) {
+  var browser = mdns.createBrowser(mdns.tcp(serviceType));
+
+  browser.on('ready', function () {
+    browser.discover();
+  });
+
+  browser.on('update', function (data) {
+    var addresses = data.addresses;
+    for (var i = 0; i < addresses.length; i++) {
+      (function(ip_address, name, protocol) {
+        var record = {
+          category: 'DEVICE',
+          ip_address: ip_address,
+          mdns_name: name,
+          mdns_protocol: protocol,
+          transport: 'ethernet',
+        };
+        callback(record);
+        resolveIpAddressToHostNameAsync(ip_address).then(function (res) {
+          record.host_name = res;
+          callback(record);
+        });
+        resolveIpAddressToMacAddressAsync(ip_address).then(function (res) {
+          record.mac_address = res;
+          record.device_type = OuiLookup.resolveMacToDeviceType(res);
+          callback(record);
+        });
+      })(addresses[i], data.type[i].name, data.type[i].protocol);
+    }
+  });
+
+  return browser;
+};
+
+exports.endDiscovery = function endDiscovery(browser /*, callback */) {
+  if (browser) {
+    browser.stop();
+  }
+};
+
+mdns.excludeInterface('0.0.0.0'); // exclude local services
+
+exports.resolveIpAddressToHostNameAsync = resolveIpAddressToHostNameAsync;
+exports.resolveIpAddressToMacAddressAsync = resolveIpAddressToMacAddressAsync;
---
+++ src/lib/usb-name-lookup.js
@@ -0,0 +1,26 @@
+'use-strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var mapping = (function() {
+  var retval = [];
+  var data = fs.readFileSync(path.resolve(__dirname, 'usb-name-mapping.csv'), 'UTF-8').split(/\r\n|\r|\n/g);
+  for (var i = 0; i < data.length; i++) {
+    var line = data[i];
+    if (line && line[0] !== '#') {
+      retval.push(line.split('\t'));
+    }
+  }
+  return retval;
+})();
+
+function resolveUsbName(id) {
+  for (var i = 0; i < mapping.length; i++) {
+    if (id.indexOf(mapping[i][0]) === 0) {
+      return mapping[i].slice(1);
+    }
+  }
+}
+
+exports.resolveUsbName = resolveUsbName;
